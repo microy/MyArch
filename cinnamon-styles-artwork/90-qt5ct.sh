8278eac708f1dd66eb01049d969c463de20c675c
Michaël Roy <microygh@gmail.com>
2024-07-05 15:30:07 +0200

    Update mint-artwork source package. Add qt5ct Xsession configuration.

--- cinnamon-styles-artwork/90-qt5ct.sh
+++ cinnamon-styles-artwork/90-qt5ct.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+# /etc/X11/xinit/xinitrc.d/90-qt5ct.sh
+
 # Enable qt5ct by default unless another PLATFORMTHEME option is set
 # or KDE is running.
 if [ -z "$QT_QPA_PLATFORMTHEME" ] && [ "$XDG_CURRENT_DESKTOP" != "KDE" ]
 then
-        export QT_QPA_PLATFORMTHEME=qt5ct:qt6ct
+        export QT_QPA_PLATFORMTHEME=qt5ct
+		export QT_ACCESSIBILITY=1
 fi
