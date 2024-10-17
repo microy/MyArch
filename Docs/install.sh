0df86bacb5498ac94e851e1ddba149ac8ea6c3b6
Michaël Roy <microygh@gmail.com>
2024-10-17 15:48:39 +0200

    Add installation script.

---
+++ Docs/install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+echo 'fr_FR.UTF-8' >> /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=fr' > /etc/vconsole.conf
+
+echo 'hostname' > /etc/hostname
+systemctl enable NetworkManager.service
+
+mkinitcpio -P
+
+bootctl install
+
+passwd
+
+useradd -m -G wheel,uucp username
+passwd username
+
+echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/wheel
+chmod -c 0440 /etc/sudoers.d/wheel
+
+echo 'Defaults env_keep += "SSH_TTY"' > /etc/sudoers.d/ssh
+chmod -c 0440 /etc/sudoers.d/ssh
+
