133f62b7fe3bd390541d33d9a49f672853fe2248
Michaël Roy <microygh@gmail.com>
2024-10-24 12:06:15 +0200

    Add some settings.

--- Config/plasma-setup.sh
+++ Config/plasma-setup.sh
@@ -19,12 +19,19 @@ kwriteconfig6 --file ksplashrc --group KSplash --key Theme none
 
 # Kscreenlocker
 kwriteconfig6 --file kscreenlockerrc --group Greeter --key WallpaperPlugin org.kde.color
-kwriteconfig6 --file kscreenlockerrc ---group Greeter --group Wallpaper --group org.kde.color --group General --key Color 46,52,54
+kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.color --group General --key Color 46,52,54
 
-# Kdeglobal
+# Kdeglobals
 kwriteconfig6 --file kdeglobals --group KDE --key AnimationDurationFactor 0
+kwriteconfig6 --file kdeglobals --group KDE --key LookAndFeelPackage org.kde.breezetwilight.desktop
+kwriteconfig6 --file kdeglobals --group KScreen --key ScaleFactor 1.25
 kwriteconfig6 --file kdeglobals --group Sounds --key Enable false
+kwriteconfig6 --file kdeglobals --group WM --key activeBackground 71,80,87
 
 # Keyboard
 kwriteconfig6 --file kxkbrc --group Layout --key Model pc105
+kwriteconfig6 --file kcminputrc --group Keyboard --key NumLock 0
+
+# Fonts
+kwriteconfig6 --file kcmfonts --group General --key forceFontDPI 
 
